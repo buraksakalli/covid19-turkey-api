bf5a1ebf88afdd8cb92d23c5243f81a62d3f417f
Burak Sakallı <buraksakalli06@gmail.com>
2021-04-08 00:51:05 +0300

    FEATURE: test added.

---
+++ test/tests.js
@@ -0,0 +1,25 @@
+const expect = require("chai").expect;
+const request = require("request");
+
+const url = {
+  weekly: "https://vakalar.herokuapp.com/weekly",
+  daily: "https://vakalar.herokuapp.com/daily",
+};
+
+describe("Weekly Data", () => {
+  it("returns status 200", (done) => {
+    request(url.weekly, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
+
+describe("Daily Data", () => {
+  it("returns status 200", (done) => {
+    request(url.daily, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
