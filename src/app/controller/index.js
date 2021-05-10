47d8ce10e06df355a9bb63c2e86632a4e1dffe52
Burak <buraksakalli06@gmail.com>
2021-05-10 18:51:40 +0300

    FEATURE: Vaccine API provides

--- src/app/controller/index.js
+++ src/app/controller/index.js
@@ -5,3 +5,5 @@ exports.weekly = (req, res) =>
   Cases.weekly((weekly) => res.status(weekly.status).json(weekly));
 exports.daily = (req, res) =>
   Cases.daily((daily) => res.status(daily.status).json(daily));
+exports.vaccine = (req, res) =>
+  Cases.vaccine((vaccine) => res.status(vaccine.status).json(vaccine));
